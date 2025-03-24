43d49118867b980e7590ea9ff71f8c9722beb443
Harsh Ray <harshray4u4@gmail.com>
2025-03-25 01:37:59 +0530

    frontend-01

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from click import option
+
+st.title('movie recommender system')
+
+ # option = st.selectbox(
+ #     'how would you like to be contacted?',
+ #     ('email','home phone','mobile phone')
+ # )
